36b53b3965cc7cd060e921f85951332666fbe527
rudrigocerqueira <rodrigocerqueirareis@gmail.com>
2026-02-27 10:48:46 -0300

    Initial Commit

---
+++ Movely/MovelyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovelyApp.swift
+//  Movely
+//
+//  Created by Rodrigo Cerqueira Reis on 27/02/26.
+//
+
+import SwiftUI
+
+@main
+struct MovelyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovelyTests/MovelyTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovelyTests.swift
+//  MovelyTests
+//
+//  Created by Rodrigo Cerqueira Reis on 27/02/26.
+//
+
+import Testing
+@testable import Movely
+
+struct MovelyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
